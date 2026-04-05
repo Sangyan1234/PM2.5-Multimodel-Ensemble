8e5fc48ea0ce8a4b7d66b11c93905d96203f55d0
Sangyan1234 <sangyanhari@gmail.com>
2026-04-05 08:31:22 +0530

    Update postprocess.py

--- src/postprocess.py
+++ src/postprocess.py
@@ -1,1 +1,9 @@
+import numpy as np
+from scipy.ndimage import gaussian_filter
 
+def smooth(pred):
+    out = np.empty_like(pred)
+    for i in range(pred.shape[0]):
+        for t in range(pred.shape[1]):
+            out[i,t] = gaussian_filter(pred[i,t], sigma=0.5)
+    return out
