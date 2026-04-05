3927c1a748c08fef2685d9db738817cb731cba77
Sangyan1234 <sangyanhari@gmail.com>
2026-04-05 08:26:23 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,16 @@
+import os
+
+FEATURES = ["cpm25","q2","t2","u10","v10","swdown","pblh","psfc","rain",
+            "PM25","NH3","SO2","NOx","NMVOC_e","NMVOC_finn","bio"]
+
+N_FEAT = len(FEATURES)
 T_IN = 10
 T_OUT = 16
 H, W = 140, 124
 
-FEATURES = [
-    "cpm25","q2","t2","u10","v10","swdown","pblh","psfc","rain",
-    "PM25","NH3","SO2","NOx","NMVOC_e","NMVOC_finn","bio"
-]
-
 EPI_STD = 3.0
 EPI_THR = 1.0
+
+VAL_FRAC = 0.20
+
+DEVICE = "cuda"
